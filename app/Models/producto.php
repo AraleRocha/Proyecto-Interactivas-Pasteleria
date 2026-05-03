9e996b5b3d6815fd60411f65f54338ba1bcf8ec9
Arale Rocha <ararepcat13@gmail.com>
2026-05-02 20:42:56 -0600

    Base de Datos - Migraciones

---
+++ app/Models/producto.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class producto extends Model
+{
+    use SoftDeletes;
+
+    protected $casts = [
+        'imagenes' => 'array'
+    ];
+
+    protected $fillable = [
+        'nombre',
+        'sabor',
+        'tamaño',
+        'categoria',
+        'precio',
+        'stock',
+        'disponible',
+        'imagen'
+    ];
+}
---
+++ database/migrations/2026_05_03_022225_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('sabor');
+            $table->string('tamano');
+            $table->string('categoria');
+            $table->decimal('precio', 10, 2);
+            $table->integer('stock')->default(0);
+            $table->boolean('disponible')->default(true);
+            $table->string('imagen')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
